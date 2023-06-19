b239a48fc8c9aca61a5a26a83b7ca6418776e4c3
shpasaa777 <88225286+shpasaa777@users.noreply.github.com>
2023-06-20 01:21:54 +0300

    Update ubuntuxmonad.sh

--- ubuntuxmonad.sh
+++ ubuntuxmonad.sh
@@ -5,6 +5,8 @@ sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install xmonad xmobar
 sudo apt-get install pcmanfm mplayer-vaapi ranger mocp atril scrot dmenu gmrun arandr compton w3m feh rxvt-unicode
 sudo apt-get install gedit vim git ubuntu-restricted-extras
+xmonad --recompile && xmonad --restart
+killall xmobar && xmobar
 # Kernel XanMod
 wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
 echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list
